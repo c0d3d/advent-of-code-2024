6b8db2b55bf7a210ea54f9583ea1f9e7c1ec2586
Neil Locketz <neil@nlocketz.com>
2024-12-11 00:01:22 -0500

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     day1::run_p1();
@@ -11,4 +12,7 @@ fn main() {
 
     day3::run_p1();
     day3::run_p2();
+
+    day4::run_p1();
+    day4::run_p2();
 }
